6eff83915693ead007ddf19ec0e62adf34f1c861
Orlando Vargas <nojipiz@protonmail.com>
2022-07-10 00:20:54 -0500

    Updated Lunarvim last config :c

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -1,60 +1,26 @@
--- GUI CONFIGURATION FOR NEOVIDE
-vim.o.guifont = "Monospace:h11"
+-- Config for Neovide
+vim.o.guifont = "Fira Code:h10"
 
--- general
+-- General
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
 lvim.keys.insert_mode["ii"] = "<Esc>"
 lvim.leader = "space"
--- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["|"] = ":lua ToggleTheme() <cr> "
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = false
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode. we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
-
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
--- }
-
--- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
+  "bash",
   "javascript",
   "json",
   "lua",
@@ -89,10 +55,6 @@ lvim.plugins = {
   },
 }
 
--- Only if i must iuser tailwind
---vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tailwindcss" })
---require("lvim.lsp.manager").setup("tailwindcss", {})
-
 -- GithubCopilot Configuration
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
@@ -111,7 +73,7 @@ lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
   end
 end
 
--- Quickchange functions
+-- Custom functions
 function ToggleTheme()
   if (vim.api.nvim_get_var("colors_name") == "onedarker") then
     vim.o.background = "light"
